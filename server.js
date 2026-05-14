28ddbd50c99e5ab3f18620728055997aa0227a9d
[raghu] <raghufks@gmail.com>
2026-05-14 10:47:03 +0530

    updated gitignordde

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/studyplanner', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://raghufks_db_user:3vsJ8eycjYDCMHPD@cluster0.cvq1otp.mongodb.net/?appName=Cluster0', {
   
 })
 .then(() => console.log('MongoDB connected successfully'))
